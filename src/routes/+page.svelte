e60812ee596394e78b5bfab956a7f18bb78df085
ifsvivek <vs63961@gmail.com>
2023-12-23 00:58:06 +0530

    You know the rules and so do I

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,87 @@
 <script>
-	import { onMount } from 'svelte';
+	const layers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-	let canvas;
-
-	onMount(() => {
-		const ctx = canvas.getContext('2d');
-		let frame;
-
-		(function loop() {
-			frame = requestAnimationFrame(loop);
-
-			const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-			for (let p = 0; p < imageData.data.length; p += 4) {
-				const i = p / 4;
-				const x = i % canvas.width;
-				const y = (i / canvas.height) >>> 0;
-
-				const t = window.performance.now();
-
-				const r = 64 + (128 * x) / canvas.width + 64 * Math.sin(t / 1000);
-				const g = 64 + (128 * y) / canvas.height + 64 * Math.cos(t / 1400);
-				const b = 128;
-
-				imageData.data[p + 0] = r;
-				imageData.data[p + 1] = g;
-				imageData.data[p + 2] = b;
-				imageData.data[p + 3] = 255;
-			}
-
-			ctx.putImageData(imageData, 0, 0);
-		})();
-
-		return () => {
-			cancelAnimationFrame(frame);
-		};
-	});
+	let y;
 </script>
 
-<canvas bind:this={canvas} width={32} height={32} />
+<svelte:window bind:scrollY={y} />
+
+<a class="parallax-container" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+	{#each layers as layer}
+		<img
+			style="transform: translate(0,{(-y * layer) / (layers.length - 1)}px)"
+			src="https://ifsvivek.github.io/host/p/parallax{layer}.png"
+			alt="parallax layer {layer}"
+		/>
+	{/each}
+</a>
+
+<div class="text">
+	<span style="opacity: {1 - Math.max(0, y / 40)}"> scroll down </span>
+
+	<div class="foreground">
+
+		You have scrolled {y} pixels
+	</div>
+</div>
 
 <style>
-	canvas {
+	.parallax-container {
+		position: fixed;
+		width: 2400px;
+		height: 712px;
+		left: 50%;
+		transform: translate(-50%, 0);
+	}
+
+	.parallax-container img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		will-change: transform;
+	}
+
+	.parallax-container img:last-child::after {
+		content: '';
+		position: absolute;
 		width: 100%;
 		height: 100%;
-		background-color: #666;
-		-webkit-mask: url(https://ifsvivek.github.io/host/r.svg) 50% 50% no-repeat;
-		mask: url(https://ifsvivek.github.io/host/r.svg) 50% 50% no-repeat;
+		background: rgb(45, 10, 13);
+	}
+
+	.text {
+		position: relative;
+		width: 100%;
+		height: 300vh;
+		color: rgb(220, 113, 43);
+		text-align: center;
+		padding: 4em 0.5em 0.5em 0.5em;
+		box-sizing: border-box;
+		pointer-events: none;
+	}
+
+	span {
+		display: block;
+		font-size: 1em;
+		text-transform: uppercase;
+		will-change: transform, opacity;
+	}
+
+	.foreground {
+		position: absolute;
+		top: 711px;
+		left: 0;
+		width: 100%;
+		height: calc(100% - 712px);
+		background-color: rgb(32, 0, 1);
+		color: white;
+		padding: 50vh 0 0 0;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background-color: rgb(253, 174, 51);
 	}
 </style>
