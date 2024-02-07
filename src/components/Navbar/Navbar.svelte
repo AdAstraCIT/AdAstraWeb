72f8a56ae0d655f83e1c2457988362bbebe777bd
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-02-07 22:49:32 +0530

    2nd commit

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,5 +1,5 @@
 <script>
-
+  import Logo from '$lib/assets/Logo.png';
 </script>
   
   <style>
@@ -12,23 +12,24 @@
     height: 50px;
     font-family: 'Montserrat', sans-serif;
     color: #fff;    
-    backdrop-filter: blur(10px) brightness(75%) saturate(80%);
+    backdrop-filter: blur(5px) brightness(75%) saturate(80%);
     box-shadow: 10px 5px 10px 5px rgba(0, 0, 0, 0.2);
     margin-top: 20px;
     border-radius: 30px;
-    width:90%;
+    width:85%;
     position: fixed;
-    left: 5%;
+    left: 7.5%;
 }
     </style>
   
-  <nav class="flex items-center justify-between px-20 navbar">
+  <nav class="flex items-center justify-between px-10 navbar">
     <ul class="list-none mr-4">
-      <li><img alt="AdAstraLogo" class="logo-img" src="src/lib/images/Logo.png"/></li>
+      <li><img alt="AdAstraLogo" class="logo-img" src={Logo}/></li>
     </ul>
-    <ul class="flex items-center ml-auto space-x-1">
-      <li><a href="/about" class="px-4 py-2">About Us</a></li>
-      <li><a href="/merch" class="px-4 py-2">Merch</a></li>
+    <ul class="flex items-center ml-auto space-x-2">
+      <li><a href="/about" class="px-1 py-2">About Us</a></li>
+      <li><span class="text-gray-400 px-2">|</span></li>
+      <li><a href="/merch" class="px-1 py-2">Merch</a></li>
     </ul>
   </nav>
   
