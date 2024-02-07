2ff791dbd85cc7bb708668a5502e4cf91a38356e
ifsvivek <vs63961@gmail.com>
2024-02-07 20:33:50 +0530

    sync

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<body
+	class="bg-black text-white bg-cover bg-no-repeat"
+	style="background-image: url('src/routes/images/IMG20240205175240.jpg')"
+></body>
+<header>
+	<div class="container mx-auto px-20 border border-green rounded-full bg-black bg-opacity-50 p-4">
+		<nav class="flex items-center justify-between px-20">
+		  <ul class="list-none mr-4">
+			<li><a href="/">AD ASTRA</a></li>
+		  </ul>
+		  <ul class="flex items-center ml-auto space-x-4">
+			<li><a href="/about" class="px-4 py-2">About US</a></li>
+			<li><a href="/merch" class="px-4 py-2">Merch</a></li>
+		  </ul>
+		</nav>
+	</div>
+</header>
+<br />
+<slot />
