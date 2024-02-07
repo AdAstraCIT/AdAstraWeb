57eda3aef2907bd99fce9a9ddbcac440b20d704b
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-02-07 22:17:53 +0530

    navbar modularized

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,13 @@
+<script>
+	import Navbar from "../components/Navbar/Navbar.svelte"; 
+</script>
+
 <body
 	class="bg-black text-white bg-cover bg-no-repeat"
-	style="background-image: url('src/routes/images/IMG20240205175240.jpg')"
+	style="background-image: url('src/lib/images/bg.png')"
 ></body>
 <header>
-	<div class="container mx-auto px-20 border border-green rounded-full bg-black bg-opacity-50 p-4">
-		<nav class="flex items-center justify-between px-20">
-		  <ul class="list-none mr-4">
-			<li><a href="/">AD ASTRA</a></li>
-		  </ul>
-		  <ul class="flex items-center ml-auto space-x-4">
-			<li><a href="/about" class="px-4 py-2">About US</a></li>
-			<li><a href="/merch" class="px-4 py-2">Merch</a></li>
-		  </ul>
-		</nav>
-	</div>
+	<Navbar />
 </header>
 <br />
 <slot />
