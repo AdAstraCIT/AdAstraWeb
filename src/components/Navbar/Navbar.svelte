01007b9f2a0969cad8f278ced8e63043802bb3cc
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-02-08 15:25:28 +0530

    responsive navbar

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,16 +1,33 @@
 <script>
 	import Logo from '$lib/assets/Logo.png';
+
+	let isOpen = false;
+
+	function toggleNav() {
+		isOpen = !isOpen;
+	}
 </script>
 
 <nav class="flex items-center justify-between px-10 navbar">
-	<ul class="list-none mr-4">
-		<li><img alt="AdAstraLogo" class="logo-img" src={Logo} /></li>
-	</ul>
-	<ul class="flex items-center ml-auto space-x-2">
-		<li><a href="/about" class="px-1 py-2">About Us</a></li>
-		<li><span class="text-gray-400 px-2">|</span></li>
-		<li><a href="/merch" class="px-1 py-2">Merch</a></li>
-	</ul>
+	<a class="navbar-brand" href="/"><img alt="AdAstraLogo" class="logo-img" src={Logo} /></a>
+</nav>
+
+<nav class="flex items-center justify-between px-10 navbar navbar-expand-md">
+	<a class="navbar-brand" href="/"><img alt="AdAstraLogo" class="logo-img" src={Logo} /></a>
+	<button class="navbar-toggler" type="button" on:click={toggleNav}>
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class:collapse={isOpen} class="navbar-collapse">
+		<ul class="flex items-center ms-auto space-x-2 md:flex">
+			<li class="nav-item">
+				<a class="nav-link" href="/about">About Us</a>
+			</li>
+			<li class="nav-item"><span class="nav-link text-gray-400 px-2">|</span></li>
+			<li class="nav-item">
+				<a href="/merch" class="nav-link px-1 py-2">Merch</a>
+			</li>
+		</ul>
+	</div>
 </nav>
 
 <style>
@@ -23,12 +40,33 @@
 		height: 50px;
 		font-family: 'Montserrat', sans-serif;
 		color: #fff;
-		backdrop-filter: blur(5px) brightness(75%) saturate(80%);
+		backdrop-filter: blur(2.5px) brightness(75%) saturate(80%);
 		box-shadow: 10px 5px 10px 5px rgba(0, 0, 0, 0.2);
-		margin-top: 20px;
+		margin-top: 25px;
 		border-radius: 30px;
 		width: 85%;
 		position: fixed;
 		left: 7.5%;
+	}
+
+	@media screen and (max-width: 768px) {
+		.logo-img {
+			height: 20px;
+			width: auto;
+		}
+	}
+
+	@media screen and (max-width: 360px) {
+		.logo-img {
+			height: 15px;
+			width: auto;
+		}
+	}
+
+	@media screen and (max-width: 300px) {
+		.logo-img {
+			height: 12px;
+			width: auto;
+		}
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,7 @@
 </header>
 <br />
 <slot />
+
+<!-- <style>
+	@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
+</style> -->
