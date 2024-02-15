d034ec7e6c990b22042d732b704ecacc32a3f140
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2024-02-16 04:22:01 +0530

    kept in raw html and css to make animation and editing easy , ping me for svelte file

---
+++ src/components/b_homepage/Homepg_Logo.svelte
@@ -0,0 +1,79 @@
+<script>
+	import Logo from '$lib/assets/Logo.png';
+</script>
+
+<div id="div_partA"><img id= "centerLogo" src="{Logo}" alt="ad_astra_Logo"/></div>
+
+<div id="div_partB">
+	<h1 class="sub_title">what is Ad Astra?</h1>
+    <p class="sub_title_dis">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, nostrum accusamus? Quis minus modi molestiae dicta aliquid nulla explicabo laborum asperiores tempore veritatis reiciendis odit nam sint, facere quibusdam dolore.</p>
+</div>
+
+<div id="div_partC">
+	<h1 class="sub_title">What we do?</h1>
+	<div id="p">
+	    <p class="sub_title_dis">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, nostrum accusamus? Quis minus modi molestiae dicta aliquid nulla explicabo laborum asperiores tempore veritatis reiciendis odit nam sint, facere quibusdam dolore.</p>
+	</div>
+	<ol id="list">
+		<li>1. First item</li>
+		<li>2. Second item</li>
+		<li>3. Third item</li>
+	</ol>
+</div>
+
+<style>
+/* the space for animations is left in the form of padding u can later change it to margin as you needs  */
+
+*{
+    margin: 0;
+    padding: 0;
+}
+
+#div_partB{
+    margin-left: 5%;
+    margin-right: 5%;
+    padding-top: 80%;
+}
+
+div{
+    display: inline-block;
+}
+
+#centerLogo{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    
+}
+
+.sub_title{
+    font-size: 280%;
+}
+
+.sub_title_dis{
+    font-size: 140%;
+    color: #8a8a8a;
+    margin-top: 2%;
+    width: 50%;
+    float: left;
+
+}
+
+#div_partC{
+    margin-left: 5%;
+    margin-right: 5%;
+    padding-top: 50%;
+    padding-bottom:20% ;
+}
+
+#list{
+    font-size: 140%;
+    color: #8a8a8a;
+
+}
+
+#p{
+    display: inline-block;
+}
+</style>
---
+++ src/components/e.fotter/33fotter.svelte
@@ -0,0 +1,52 @@
+<script>
+	import Logo from '$lib/assets/Logo.png';
+</script>
+<div id="logo_part">
+    <img id="logo" src="{Logo}" alt="">
+</div>
+
+<div id="footbar">
+    <!-- icon of twitter  -->
+    <i class="fa-brands fa-twitter"></i>
+    <!-- icon of linkedin  -->
+    <i class="fa-brands fa-linkedin"></i>
+    <!-- icon of mail  -->
+    <i class="fa-solid fa-envelope">hello@examle.com</i>
+    <!-- icon of phone  -->
+    <i class="fa-solid fa-phone">+91 00000 00000</i>
+
+    <a href="#">Privacy Policy</a>
+
+    <a href="#">Terms of Service (bar ,still in progress)</a>
+</div>
+<!-- you can use font osm for icon of twitter and linkedin 
+and font awesome for icon of mail and phone 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    add class as in and x for linkedin and twitter -->
+
+<style>
+    #logo{
+        height: 80px;
+        width: auto;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    #logo_part{
+        margin-top: 5px;
+        border-top: rgb(255, 255, 255) 1px solid;
+        border-bottom: rgb(255, 255, 255) 1px solid;
+    }
+
+    /* #footbar{
+        
+         */
+
+    
+</style>
