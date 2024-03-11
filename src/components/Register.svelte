c06f49db77cdcc417ad5472788de4f233b88997f
ifsvivek <vs63961@gmail.com>
2024-03-11 23:06:13 +0530

    Update Register component styles and add RegisterBg import

--- src/components/Register.svelte
+++ src/components/Register.svelte
@@ -1,11 +1,16 @@
+<script>
+	import { RegisterBg } from '$lib';
+</script>
+
 <main
 	class="overflow-hidden p-20 rounded-3xl border border-solid bg-blend-screen border-neutral-800 max-md:px-5 max-md:max-w-full mx-20 mt-32"
+	style="background-image: url({RegisterBg}); background-size: cover; background-position: center;"
 >
 	<section class="flex gap-5 max-md:flex-col max-md:gap-0">
 		<div class="flex flex-col w-[86%] max-md:ml-0 max-md:w-full">
 			<article class="flex flex-col max-md:mt-10 max-md:max-w-full">
-				<h1 class="text-4xl text-lime-400 leading-[60px] max-md:max-w-full">
-					Start your journey with <span class="text-lime-400">Ad Astra today!</span>
+				<h1 class="text-4xl text-[#CAFF33] leading-[60px] max-md:max-w-full">
+					Start your journey with <span class="text-[#CAFF33]">Ad Astra today!</span>
 				</h1>
 				<p class="mt-3.5 text-lg font-light leading-7 text-zinc-400 max-md:max-w-full">
 					Lorem ipsum, or lipsum as it is sometimes known, is text used in laying out print, graphic
@@ -16,7 +21,7 @@
 		</div>
 		<div class="flex flex-col ml-5 w-[14%] max-md:ml-0 max-md:w-full">
 			<button
-				class="justify-center self-stretch px-8 py-5 my-auto w-full text-lg leading-7 whitespace-nowrap bg-lime-400 rounded-[82px] text-zinc-900 max-md:px-5 max-md:mt-10"
+				class="justify-center self-stretch px-8 py-5 my-auto w-full text-lg leading-7 whitespace-nowrap bg-[#CAFF33] rounded-[82px] text-zinc-900 max-md:px-5 max-md:mt-10"
 				type="button">Register</button
 			>
 		</div>
