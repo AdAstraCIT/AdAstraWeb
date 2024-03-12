d9318982a6b13439d49c7f1f3239a16fe6530aed
ifsvivek <vs63961@gmail.com>
2024-03-12 15:33:00 +0530

    Fix formatting issues in Background and Infinity components

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,11 +1,11 @@
 <script>
-    import { back1 } from '$lib';
+	import { back1 } from '$lib';
 </script>
 
 <div class="flex justify-center items-center h-screen">
-    <img
-        src={back1}
-        class="absolute top-0 left-0 w-full h-full object-cover z-[-1] self-center mx-auto"
-        alt="background"
-    />
+	<img
+		src={back1}
+		class="absolute top-0 left-0 w-full h-full object-cover z-[-1] self-center mx-auto"
+		alt="background"
+	/>
 </div>
--- src/components/Infinity.svelte
+++ src/components/Infinity.svelte
@@ -4,7 +4,7 @@
 		xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink"
 		width="100%"
-        height="100%"
+		height="100%"
 		viewBox="0 0 594 215"
 	>
 		<g class="st2">
