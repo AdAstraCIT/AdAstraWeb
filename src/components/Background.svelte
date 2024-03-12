abe581bf2da0375bf73be5ead2561516f7239a6e
P L VIJAYA VITTHAL <146193592+vijay1356@users.noreply.github.com>
2024-03-12 12:40:32 +0530

    Update Background.svelte

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -1,14 +1,10 @@
 <script>
-    import { video } from '$lib';
+    import { back1 } from '$lib';
 </script>
 
 <div class="flex justify-center items-center h-screen">
-    <video
-        src={video}
-        autoplay
-        muted
-        loop
-        playsinline
+    <img
+        src={back1}
         class="fixed top-0 left-0 w-full h-full object-cover z-[-1]"
     />
 </div>
