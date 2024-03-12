f5551b8777d6d66ece7cf8f3669e02ce91b276cd
K V Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-03-12 14:28:34 +0530

    footbar text hide

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -22,28 +22,28 @@
 			href="https://twitter.com/adastracit"
 			target="_blank"
 			rel="noopener noreferrer"
-			class="p-1 rounded-full"><img src={twitter} alt="Twitter" /></a
+			class="p-1 rounded-full text-white mx-3 object-contain"><img src={twitter} alt="Twitter" /></a
 		>
 		<a
 			href="https://www.linkedin.com/company/adastracit/"
 			target="_blank"
 			rel="noopener noreferrer"
-			class="p-1 rounded-full"><img src={linkedin} alt="LinkedIn" /></a
+			class="p-1 rounded-full text-white mx-3 object-contain"><img src={linkedin} alt="LinkedIn" /></a
 		>
-		<div class="hidden md:flex items-center space-x-4">
+		<div class="flex items-center space-x-4">
 			<a class="text-sm inline-flex" href="mailto:adastra@cambridge.edu.in">
-				<img class="text-white mx-3" src={email} alt="Email icon" />
-				<span class="text-white">adastra@cambridge.edu.in</span>
+				<img class="text-white mx-3 object-contain" src={email} alt="Email icon" />
+				<span class="text-white hidden md:block">adastra@cambridge.edu.in</span>
 			</a>
 			<a href="tel:+910000000000">
-				<img src={phone} alt="Phone icon" />
-				<span class="text-white">+91 00000 00000</span>
+				<img class="text-white mx-3 object-contain" src={phone} alt="Phone icon" />
+				<span class="text-white hidden md:block">+91 00000 00000</span>
 			</a>
 		</div>
-	</div>
-	<ul class="flex items-center ms-auto space-x-2 mr-4">
-		<li><a href="/Privacy" class="px-1 py-2 hover:text-gray-300">Privacy Policy</a></li>
-		<li><span class="text-gray-400 px-2">|</span></li>
-		<li><a href="/tos" class="px-1 py-2 hover:text-gray-300">Terms of Service</a></li>
+	</div>	
+	<ul class="flex items-center ms-auto space-x-2 mr-4 text-white hidden md:block">
+		<li><a href="/Privacy" class="px-1 py-2 hover:text-gray-300 text-white hidden md:block">Privacy Policy</a></li>
+		<li><span class="text-gray-400 px-2 hidden md:block">|</span></li>
+		<li><a href="/tos" class="px-1 py-2 hover:text-gray-300 text-white hidden md:block">Terms of Service</a></li>
 	</ul>
 </div>
